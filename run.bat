364db5626cfdce65b2b5c45dfed4a311af393069
Benjamin Carter <59350660+BenRobotics101@users.noreply.github.com>
2022-12-03 18:16:12 -0700

    Make relative paths general

--- run.bat
+++ run.bat
@@ -1,13 +1,13 @@
-"C:/Program Files/Python38/python.exe" "c:/Users/BenCarter/Documents/Bens Work/Coding/Python/PhotoArt/colorMapping.py"
+python colorMapping.py
 echo moon.jpg > selection.txt
 echo "Move screen to right size"
 pause
-"C:/Program Files/Python38/python.exe" "c:/Users/BenCarter/Documents/Bens Work/Coding/Python/PhotoArt/photoSlicerColorHSL.py"
-"C:/Users/BenCarter/Documents/Bens Work/Coding/Python/PhotoArt/PrettyConsole.exe"
+python photoSlicerColorHSL.py
+PrettyConsole.exe
 pause
-"C:/Program Files/Python38/python.exe" "c:/Users/BenCarter/Documents/Bens Work/Coding/Python/PhotoArt/photoSlicerColor.py"
-"C:/Users/BenCarter/Documents/Bens Work/Coding/Python/PhotoArt/PrettyConsole.exe"
+python photoSlicerColor.py
+PrettyConsole.exe
 pause
-"C:/Program Files/Python38/python.exe" "c:/Users/BenCarter/Documents/Bens Work/Coding/Python/PhotoArt/photoSlicer.py"
-"C:/Users/BenCarter/Documents/Bens Work/Coding/Python/PhotoArt/PrettyConsoleBW.exe"
+python photoSlicer.py
+PrettyConsoleBW.exe
 pause
